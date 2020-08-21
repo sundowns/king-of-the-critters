c2635eb9ccf594c28ab11be50b9e5efcdd579a2b
Tom <tom@smallridge.com.au>
2020-08-21 17:54:12 +0800

    minor menu tweaks

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -1,6 +1,11 @@
 extends Control
 
 onready var play_button: Button = $PlayButton
+onready var controls_label: Label = $Controls
+
+func _ready():
+	$Crown/AnimationPlayer.play("Dialogue")
+
 
 func _on_PlayButton_pressed():
 	Global.start_game()
