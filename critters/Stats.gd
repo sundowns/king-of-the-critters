106b8e225bcb28c1dfa200527137881ec42d5771
Tom <tom@smallridge.com.au>
2020-08-22 18:41:20 +0800

    health updates

--- critters/Stats.gd
+++ critters/Stats.gd
@@ -9,9 +9,9 @@ onready var health = max_health
 signal no_health
 
 func set_health(new_health):
-	health = new_health
+	health = min(new_health, max_health)
 
 func take_damage(damage):
-	health -= damage
+	set_health(health - damage)
 	if health <= 0:
 		emit_signal("no_health")
